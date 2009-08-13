7402fdc9686c51bc67ea248cc0fb27a202a03587
Masayoshi Takahashi <takahashimm@gmail.com>
2009-08-14 03:09:20 +0900

    add wiki name

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,7 @@
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+##RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -39,3 +40,5 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 end
+
+WIKI_NAME='Recipe Wiki'
