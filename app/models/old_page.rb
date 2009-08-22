db38c47f0b4758d3480db0a4651678080125d95e
Masayoshi Takahashi <takahashimm@gmail.com>
2009-08-22 12:15:11 +0900

    fix prop names for validation

--- app/models/old_page.rb
+++ app/models/old_page.rb
@@ -1,6 +1,6 @@
 class OldPage < ActiveRecord::Base
   validates_presence_of :page_id
-  validates_presence_of :path
-  validates_presence_of :title
-  validates_presence_of :content
+  validates_presence_of :page_path
+  validates_presence_of :page_title
+  validates_presence_of :page_content
 end
