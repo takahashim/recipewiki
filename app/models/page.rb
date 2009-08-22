a7d41d6bef871e08443ce72a11ce373df1e105ee
Masayoshi Takahashi <takahashimm@gmail.com>
2009-08-22 12:15:58 +0900

    save OldPage when save Page

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,4 +2,18 @@ class Page < ActiveRecord::Base
   validates_presence_of :path
   validates_presence_of :title
   validates_presence_of :content
+
+  def before_save
+    logger.info 'hoge'
+    old_page = OldPage.new
+    old_page.page_id = self.id
+    old_page.page_title = self.title
+    old_page.page_path = self.path
+    old_page.page_content = self.content
+    old_page.page_created_at = self.created_at
+    old_page.page_updated_at = self.updated_at
+    logger.info self.path
+    old_page.save!
+    true
+  end
 end
