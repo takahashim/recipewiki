22ac1e4c720841b1fbbe5301e6cb340449f31597
Masayoshi Takahashi <takahashimm@gmail.com>
2009-08-22 12:14:15 +0900

    modified old_pages table

--- db/migrate/20090817063643_create_old_pages.rb
+++ db/migrate/20090817063643_create_old_pages.rb
@@ -2,9 +2,9 @@ class CreateOldPages < ActiveRecord::Migration
   def self.up
     create_table :old_pages do |t|
       t.integer :page_id
-      t.string :title
-      t.string :path
-      t.text :content
+      t.string :page_title
+      t.string :page_path
+      t.text :page_content
       t.datetime :page_created_at
       t.datetime :page_updated_at
       t.timestamps
